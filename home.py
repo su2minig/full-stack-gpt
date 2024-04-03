1e871fc5d6623b68342bdf92fa23369060fbd67c
su2minig <mini72667@gmail.com>
2024-04-03 15:30:30 +0900

    #7.1

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain.prompts import PromptTemplate
 
 st.title("Hello world!")
 
@@ -9,3 +10,18 @@ st.markdown(
     This is a simple example of a streamlit app.
     """
 )
+
+st.write("hello")
+
+st.write([1,2,3,4])
+
+st.write({"x": 1})
+
+st.write(PromptTemplate)
+
+p = PromptTemplate.from_template("xxxx")
+
+st.write(p)
+
+
+st.selectbox("CHoose your model", ("GPT-3","GPT-4"),)
