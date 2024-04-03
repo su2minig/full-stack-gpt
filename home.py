a1606c994918cda53f43d232e9fe1e36fe1d77db
su2minig <mini72667@gmail.com>
2024-04-03 16:08:47 +0900

    #7.4

--- home.py
+++ home.py
@@ -1,27 +1,8 @@
 import streamlit as st
-from datetime import datetime
 
-today = datetime.today().strftime("%H:%M:%S")
-
-st.title(today)
-
-model = st.selectbox(
-  "CHoose your model",
-  (
-    "GPT-3","GPT-4"
-  ),
+st.set_page_config(
+  page_title="Fullstack GPT",
+  page_icon="🧊",
 )
 
-st.write(model)
-
-name = st.text_input("What is your name?")
-
-st.write(name)
-
-value = st.slider(
-  "temperature",
-  min_value=0.1,
-  max_value=1.0
-)
-
-st.write(value)
+st.title("FUllstack Gpt Home")
